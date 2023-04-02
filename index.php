3e2b5b564e39a7e060a392c046b6b2443981bf4b
Diego Lamana <126724083+diegoCodeJavascript@users.noreply.github.com>
2023-04-02 17:36:01 -0300

    Código da aplicação
    Este é o index, responsável da aplicação com o código e função em php para obter o número do ip, 
    do dispositivo celular ou computador, que fez o acesso a página index,   sobre os demais dados como latitude e longitude, é obtido via,
    Javascript, e adicionada  no input, caso o usuário der permissão em passar as informações de localização do dispositivo,

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Qual Ip</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+  
+  <div id="caixaprimaria">
+      <img src="4458558.png" id="mgqualipilustrar" />
+      <h2 id="titulopg">Qual IP </h2>
+      <h4 id="slogansite">Obtenha informações da sua conexão</h4>
+  </div>
+  <form method="POST" action="processar-informacao.php">
+    <label id="txtlabel1">Seu IP:</label>
+    <input id="cxipmostrar" type="text" name="seuip">
+    <input id="btnpegardados" type="submit" name="obterdados">
+  </form>
+  <p id="txtqualipinformation">Com o Qual IP, você pode descobrir facilmente o seu endereço IP e a sua geolocalização em apenas alguns cliques. Nosso serviço é fácil de usar e fornece informações precisas sobre a sua conexão, permitindo que você tenha controle total da sua privacidade online.<br/><br/>
+
+Além disso, o Qual IP é uma ferramenta útil para quem trabalha com tecnologia, permitindo que você acesse informações importantes sobre a sua conexão de forma rápida e prática. Com nosso serviço, você pode ter certeza de que está sempre informado sobre a sua conexão e pode tomar medidas para proteger a sua privacidade e segurança na internet.<br/><br/>
+</p>
+</body>
+</html>
